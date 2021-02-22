8b1629fbc6c67774a57de6ce63ce93c3f23192ba
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-22 19:17:43 -0300

    Criando primeiras rotas api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
  
+app.get("/", (Request, response) =>{
+    return response.send("olá! NLW-04")
+})
+
+app.post("/", (Request, response) =>{
+    return response.json("os dados foram salvos com sucesso!")
+})
+
 app.listen(3333, () => console.log("server is running!"));
